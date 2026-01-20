a1c68ba415d96ff3ac8f3666cc590c63a7a91314
bogdanost <bo12@kent.ac.uk>
2026-01-20 13:06:21 +0000

    workshop2sect2

--- workshop2part1.py
+++ workshop2part1.py
@@ -2,6 +2,6 @@ def age_confirm():
     age = int(input("Enter your age: "))
     if age >= 18:
         print("You are an adult")
-    else:
+    if 0<=age<18:
         print("You are a child")
 age_confirm()
