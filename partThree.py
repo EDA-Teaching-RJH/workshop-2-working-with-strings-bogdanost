cc26c58cc7f5d5abd2798d6adb87943b943f869b
bogdanost <bo12@kent.ac.uk>
2026-01-20 12:55:43 +0000

    workshop 2 part 3

--- partThree.py
+++ partThree.py
@@ -3,12 +3,18 @@ def main():
     percent = percent_to_float(input("What percentage would you like to charge? "))
     charge = pounds * percent
     print(f"Charge £{charge:.2f}")
+    
 
 
 def pounds_to_float(d):
-    # TODO
+    new = d.replace("£", "")
+    new = float(new)
+    return new
 
 def percent_to_float(p):
-    # TODO
+    new = p.replace("%", "")
+    new = int(new)
+    new = new/100
+    return new
 
 main()
